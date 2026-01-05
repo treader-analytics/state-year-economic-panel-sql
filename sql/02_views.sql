7d2930d487c0c97770e5fccc0ab8dcd2e9089416
Trevor Reader <treader317@gmail.com>
2026-01-05 00:42:45 -0500

    add analysis-ready panel view

--- sql/02_views.sql
+++ sql/02_views.sql
@@ -1,1 +1,17 @@
+-- 02_views.sql
+-- Analysis-ready view (final deliverable)
 
+CREATE OR REPLACE VIEW econ.vw_state_year_panel AS
+SELECT
+  s.state_fips,
+  s.state_abbr,
+  s.state_name,
+  f.year,
+  f.poverty_rate,
+  f.median_income,
+  f.unemployment_rate,
+  f.poverty_population,
+  f.labor_force
+FROM econ.fact_state_year f
+JOIN econ.dim_state s
+  ON s.state_fips = f.state_fips;
