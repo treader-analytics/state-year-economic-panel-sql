a7d49188389aabd53edc163e7597dd8aca44913d
Trevor Reader <treader317@gmail.com>
2026-01-05 00:43:21 -0500

    add indexes for common query patterns

--- sql/03_indexes.sql
+++ sql/03_indexes.sql
@@ -1,1 +1,8 @@
+-- 03_indexes.sql
+-- Performance indexes for common filters and joins
 
+CREATE INDEX IF NOT EXISTS ix_fact_year
+ON econ.fact_state_year (year);
+
+CREATE INDEX IF NOT EXISTS ix_fact_state_year
+ON econ.fact_state_year (state_fips, year);
